01189521ff1ab2a15a8ad705fa9c080687b822c4
Jago128 <jagobaetxegarai@gmail.com>
2025-10-08 14:14:04 +0200

    Started work on the client side.

---
+++ src/threads/ClientThread.java
@@ -0,0 +1,14 @@
+package threads;
+
+public class ClientThread implements Runnable {
+	private Usuario user;
+	
+	public ClientThread(Usuario user) {
+		this.user = user;
+	}
+
+	@Override
+	public void run() {
+		
+	}
+}
