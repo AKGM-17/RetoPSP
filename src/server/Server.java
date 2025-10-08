484771f19a2b16b25676b56acc554b36c83ec816
Jago128 <jagobaetxegarai@gmail.com>
2025-10-08 13:36:24 +0200

    e

---
+++ src/server/Server.java
@@ -0,0 +1,50 @@
+package server;
+
+import java.io.*;
+import java.net.*;
+
+public class Server {
+	private final int PUERTO = 5000;
+
+	public void iniciar() {
+		Socket cliente = null;
+		ObjectInputStream entrada = null;
+		ObjectOutputStream salida = null;
+		try (ServerSocket servidor = new ServerSocket(PUERTO)) {
+			while (true) {
+				System.out.println("Esperando conexiones del cliente...");
+
+				try {
+					cliente = servidor.accept();
+					System.out.println("Cliente conectado.");
+					salida = new ObjectOutputStream(cliente.getOutputStream());
+					entrada = new ObjectInputStream(cliente.getInputStream());
+
+					salida.writeObject("Hola ¡Cliente!");
+				} catch (Exception e) {
+					System.out.println("Error: " + e.getMessage());
+				} finally {
+					try {
+						if (entrada != null)
+							entrada.close();
+						if (salida != null)
+							salida.close();
+						if (cliente != null)
+							cliente.close();
+					} catch (Exception e) {
+						System.out.println("Error: " + e.getMessage());
+					}
+				}
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void main(String[] args) {
+		Server s = new Server();
+		s.iniciar();
+	}
+}
