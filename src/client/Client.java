4e719f423fcbec6fb608e7814b96d07dbf5831b7
AKGM-17 <ekaintorresmiranda@gmail.com>
2025-10-08 13:18:09 +0200

    client+server

---
+++ src/client/Client.java
@@ -0,0 +1,23 @@
+package client;
+
+import java.awt.EventQueue;
+
+import view.LogInView;
+
+public class Client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LogInView frame = new LogInView();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+}
--- src/view/LogInView.java
+++ src/view/LogInView.java
@@ -22,18 +22,7 @@ public class LogInView extends JFrame implements ActionListener{
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					LogInView frame = new LogInView();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+
 
 	/**
 	 * Create the frame.
