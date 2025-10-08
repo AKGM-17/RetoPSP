128485e345ed253d3a1176d2348d4a5f8fbff0f7
AKGM-17 <ekaintorresmiranda@gmail.com>
2025-10-08 13:47:37 +0200

    modified:   bin/client/Client$1.class 	modified:   bin/client/Client.class 	modified:   bin/server/Server.class 	modified:   src/client/Client.java 	modified:   src/server/Server.java

--- src/server/Server.java
+++ src/server/Server.java
@@ -8,49 +8,47 @@ import java.net.Socket;
 
 public class Server {
 	private final int PUERTO = 5000;
-    public void iniciar() {
-        //ServerSocket servidor = null;
-        Socket cliente = null;
-        ObjectInputStream entrada = null;
-        ObjectOutputStream salida = null;
-        try ( ServerSocket servidor = new ServerSocket(PUERTO))
-        {
-        	while (true) 
-        	{
-	            System.out.println("Esperando conexiones del cliente...");
-	            
-	            try
-	            {
-		            cliente = servidor.accept();
-		            System.out.println("Cliente conectado.");
-		            salida = new ObjectOutputStream (cliente.getOutputStream());
-		            entrada = new ObjectInputStream(cliente.getInputStream());
-		           
-		
-		            salida.writeObject("Hola ¡Cliente!");
-	            } catch (Exception e) {
-	            	System.out.println("Error: " + e.getMessage());
-	            } finally {
-	            	try {	 
-  				if (entrada != null)
-		                    entrada.close();
-		                if (salida != null)
-		                    salida.close();           		
-		                if (cliente != null)
-		                    cliente.close();             
-	            	} catch (Exception e) {
-	            		System.out.println("Error: " + e.getMessage());
-	            	}
-	            }
-        	}
-        
-    	} catch (IOException e) {
-        e.printStackTrace();
-    	}   
 
-    }
-    public static void main(String[] args) {
-        Server s = new Server();
-        s.iniciar();
-    }
+	public void iniciar() {
+		// ServerSocket servidor = null;
+		Socket cliente = null;
+		ObjectInputStream entrada = null;
+		ObjectOutputStream salida = null;
+		try (ServerSocket servidor = new ServerSocket(PUERTO)) {
+			while (true) {
+				System.out.println("Esperando conexiones del cliente...");
+
+				try {
+					cliente = servidor.accept();
+					System.out.println("Cliente conectado.");
+					salida = new ObjectOutputStream(cliente.getOutputStream());
+					entrada = new ObjectInputStream(cliente.getInputStream());
+
+					salida.writeObject("Hola ¡Cliente!");
+				} catch (Exception e) {
+					System.out.println("Error: " + e.getMessage());
+				} finally {
+					try {
+						if (entrada != null)
+							entrada.close();
+						if (salida != null)
+							salida.close();
+						if (cliente != null)
+							cliente.close();
+					} catch (Exception e) {
+						System.out.println("Error: " + e.getMessage());
+					}
+				}
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void main(String[] args) {
+		Server s = new Server();
+		s.iniciar();
+	}
 }
