3b8232c3a4b45b409cec3255a6e34d492fdaac49
Jago128 <jagobaetxegarai@gmail.com>
2025-10-08 14:00:01 +0200

    e

--- src/client/Client.java
+++ src/client/Client.java
@@ -7,7 +7,6 @@ import view.LogInView;
 public class Client {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
